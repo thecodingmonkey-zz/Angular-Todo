b06102b45e0d0ca94d8f0678bde77a43c3fc1ed3
Andrew Wong <coding.monkey.andrew@gmail.com>
2015-03-09 11:21:51 -1000

    blah

--- js/app.js
+++ js/app.js
@@ -4,11 +4,20 @@ angular
   // dependency injection?                     v
   .controller('TodoController', ['$scope', function($scope) {
 
+
     $scope.save_todo = function( new_title ) {
       $scope.todos.push({
           title: new_title,
           completed : false
       });
+    }
+
+    $scope.enter_saves = function ($event) {
+      console.log($event);
+
+      if ( $event.keyCode == 13) {
+        $scope.save_todo( $scope.new_todo );
+      }
     }
 
 //    $scope.name = "IS HOOKED UP";
