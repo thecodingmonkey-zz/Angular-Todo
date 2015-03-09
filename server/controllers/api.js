89e02a7a6723addda6ce65255ca16cb81a224977
Andrew Wong <coding.monkey.andrew@gmail.com>
2015-03-09 12:01:00 -1000

    non-panic mode, for now

---
+++ server/controllers/api.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+
+// list all
+router.get('/', function (req, res) {
+  res.send("HELLO API");
+})
+
+// add
+
+// delete
+
+// complete
+
+// undelete
+
+module.exports = router;
